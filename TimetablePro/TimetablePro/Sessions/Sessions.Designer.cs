6cc02ad578f899a1799666e9bfdf6229b19a42ca
Dinithi <dinithimb@gmail.com>
2020-09-20 22:58:25 +0530

    Sessions page added.

---
+++ TimetablePro/TimetablePro/Sessions/Sessions.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace TimetablePro
+{
+    partial class SessionsManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpt1 = new System.Windows.Forms.Button();
+            this.btnOpt2 = new System.Windows.Forms.Button();
+            this.btnOpt3 = new System.Windows.Forms.Button();
+            this.btnOpt4 = new System.Windows.Forms.Button();
+            this.btnOpt5 = new System.Windows.Forms.Button();
+            this.btnOpt6 = new System.Windows.Forms.Button();
+            this.btnOpt8 = new System.Windows.Forms.Button();
+            this.btnOpt9 = new System.Windows.Forms.Button();
+            this.btnOpt7 = new System.Windows.Forms.Button();
+            this.btnOpt10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnOpt11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpt1
+            // 
+            this.btnOpt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt1.Location = new System.Drawing.Point(9, 120);
+            this.btnOpt1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt1.Name = "btnOpt1";
+            this.btnOpt1.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt1.TabIndex = 6;
+            this.btnOpt1.Text = "Home";
+            this.btnOpt1.UseVisualStyleBackColor = true;
+            this.btnOpt1.Click += new System.EventHandler(this.btnOpt1_Click);
+            // 
+            // btnOpt2
+            // 
+            this.btnOpt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt2.Location = new System.Drawing.Point(9, 158);
+            this.btnOpt2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt2.Name = "btnOpt2";
+            this.btnOpt2.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt2.TabIndex = 6;
+            this.btnOpt2.Text = "Student Management";
+            this.btnOpt2.UseVisualStyleBackColor = true;
+            this.btnOpt2.Click += new System.EventHandler(this.btnOpt2_Click);
+            // 
+            // btnOpt3
+            // 
+            this.btnOpt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt3.Location = new System.Drawing.Point(9, 196);
+            this.btnOpt3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt3.Name = "btnOpt3";
+            this.btnOpt3.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt3.TabIndex = 6;
+            this.btnOpt3.Text = "Lecturer Management";
+            this.btnOpt3.UseVisualStyleBackColor = true;
+            this.btnOpt3.Click += new System.EventHandler(this.btnOpt3_Click);
+            // 
+            // btnOpt4
+            // 
+            this.btnOpt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt4.Location = new System.Drawing.Point(9, 234);
+            this.btnOpt4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt4.Name = "btnOpt4";
+            this.btnOpt4.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt4.TabIndex = 6;
+            this.btnOpt4.Text = "Subject Management";
+            this.btnOpt4.UseVisualStyleBackColor = true;
+            this.btnOpt4.Click += new System.EventHandler(this.btnOpt4_Click);
+            // 
+            // btnOpt5
+            // 
+            this.btnOpt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt5.Location = new System.Drawing.Point(9, 271);
+            this.btnOpt5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt5.Name = "btnOpt5";
+            this.btnOpt5.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt5.TabIndex = 6;
+            this.btnOpt5.Text = "Tags Management";
+            this.btnOpt5.UseVisualStyleBackColor = true;
+            // 
+            // btnOpt6
+            // 
+            this.btnOpt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt6.Location = new System.Drawing.Point(9, 310);
+            this.btnOpt6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt6.Name = "btnOpt6";
+            this.btnOpt6.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt6.TabIndex = 6;
+            this.btnOpt6.Text = "Location Management";
+            this.btnOpt6.UseVisualStyleBackColor = true;
+            this.btnOpt6.Click += new System.EventHandler(this.btnOpt6_Click);
+            // 
+            // btnOpt8
+            // 
+            this.btnOpt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt8.Location = new System.Drawing.Point(9, 379);
+            this.btnOpt8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt8.Name = "btnOpt8";
+            this.btnOpt8.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt8.TabIndex = 6;
+            this.btnOpt8.Text = "Working days/Hours";
+            this.btnOpt8.UseVisualStyleBackColor = true;
+            this.btnOpt8.Click += new System.EventHandler(this.btnOpt8_Click);
+            // 
+            // btnOpt9
+            // 
+            this.btnOpt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt9.Location = new System.Drawing.Point(9, 413);
+            this.btnOpt9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt9.Name = "btnOpt9";
+            this.btnOpt9.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt9.TabIndex = 6;
+            this.btnOpt9.Text = "Statistics";
+            this.btnOpt9.UseVisualStyleBackColor = true;
+            this.btnOpt9.Click += new System.EventHandler(this.btnOpt9_Click);
+            // 
+            // btnOpt7
+            // 
+            this.btnOpt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt7.Location = new System.Drawing.Point(9, 343);
+            this.btnOpt7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt7.Name = "btnOpt7";
+            this.btnOpt7.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt7.TabIndex = 6;
+            this.btnOpt7.Text = "Sessions Management";
+            this.btnOpt7.UseVisualStyleBackColor = true;
+            // 
+            // btnOpt10
+            // 
+            this.btnOpt10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt10.Location = new System.Drawing.Point(9, 446);
+            this.btnOpt10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt10.Name = "btnOpt10";
+            this.btnOpt10.Size = new System.Drawing.Size(179, 28);
+            this.btnOpt10.TabIndex = 6;
+            this.btnOpt10.Text = "Help";
+            this.btnOpt10.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(938, 91);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TimetablePro.Properties.Resources.I21;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(86, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(108, 70);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(234, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "@copyright 2020 All rights reserved";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(108, 39);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Team Cloves";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Harrington", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(102, 6);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(225, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Timetable Pro V1.1";
+            // 
+            // btnOpt11
+            // 
+            this.btnOpt11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpt11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpt11.Location = new System.Drawing.Point(9, 479);
+            this.btnOpt11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpt11.Name = "btnOpt11";
+            this.btnOpt11.Size = new System.Drawing.Size(179, 31);
+            this.btnOpt11.TabIndex = 8;
+            this.btnOpt11.Text = "Generate Timetable";
+            this.btnOpt11.UseVisualStyleBackColor = true;
+            // 
+            // SessionsManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(995, 551);
+            this.Controls.Add(this.btnOpt11);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnOpt10);
+            this.Controls.Add(this.btnOpt7);
+            this.Controls.Add(this.btnOpt9);
+            this.Controls.Add(this.btnOpt6);
+            this.Controls.Add(this.btnOpt5);
+            this.Controls.Add(this.btnOpt8);
+            this.Controls.Add(this.btnOpt3);
+            this.Controls.Add(this.btnOpt4);
+            this.Controls.Add(this.btnOpt2);
+            this.Controls.Add(this.btnOpt1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "SessionsManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timetable Pro V1.1";
+            //this.Load += new System.EventHandler(this.Sessions);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpt1;
+        private System.Windows.Forms.Button btnOpt2;
+        private System.Windows.Forms.Button btnOpt3;
+        private System.Windows.Forms.Button btnOpt4;
+        private System.Windows.Forms.Button btnOpt5;
+        private System.Windows.Forms.Button btnOpt6;
+        private System.Windows.Forms.Button btnOpt8;
+        private System.Windows.Forms.Button btnOpt9;
+        private System.Windows.Forms.Button btnOpt7;
+        private System.Windows.Forms.Button btnOpt10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnOpt11;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TimetablePro/TimetablePro/Sessions/Sessions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimetablePro
+{
+    public partial class SessionsManagement : Form
+    {
+        SqlConnection sqlcon = new SqlConnection(@"Server=tcp:timetableserver2020.database.windows.net,1433;Initial Catalog=TimetableDB;Persist Security Info=False;User ID=demo;Password=myAzure1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        public SessionsManagement()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void btnOpt2_Click(object sender, EventArgs e)
+        {
+            studentGroupsManagement studentGroupsManagement = new studentGroupsManagement();
+
+            this.Hide();
+            studentGroupsManagement.Show();
+        }
+
+        private void btnOpt1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnOpt3_Click(object sender, EventArgs e)
+        {
+            AddNewLecturer addNewLecturer = new AddNewLecturer();
+
+            this.Hide();
+            addNewLecturer.Show();
+        }
+
+        private void btnOpt4_Click(object sender, EventArgs e)
+        {
+            AddSubjectDetails addSubjectDetails = new AddSubjectDetails();
+
+            this.Hide();
+            addSubjectDetails.Show();
+        }
+
+
+        private void btnOpt6_Click(object sender, EventArgs e)
+        {
+            Location1 location1 = new Location1();
+
+            this.Hide();
+            location1.Show();
+        }
+
+        private void btnOpt9_Click(object sender, EventArgs e)
+        {
+            StatisticsForm statisticsForm = new StatisticsForm();
+
+            this.Hide();
+            statisticsForm.Show();
+
+        }
+
+        private void btnOpt8_Click(object sender, EventArgs e)
+        {
+            WorkingDays workingDays = new WorkingDays();
+
+            this.Hide();
+            workingDays.Show();
+        }
+    }
+}
